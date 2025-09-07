7a7bd2bb33c91b07c49cfe6d324c5eaf7b590653
Markus <marku.stenli.47@gmail.com>
2025-09-08 01:40:41 +0300

    второй урок начало

--- src/main/java/ru/otus/java/basic/home/work/Application.java
+++ src/main/java/ru/otus/java/basic/home/work/Application.java
@@ -1,4 +1,25 @@
 package ru.otus.java.basic.home.work;
 
 public class Application {
-}
+    public static void main(String[] args) {
+       one();
+       two();
+        }
+
+        public static void one () {
+             for (int i = 1; i <= 3; i++) {
+             System.out.println(i + "java");
+             }
+        }
+
+        public static void two (){
+            for (int i = 1; i <= 10; i++) {
+                if (i > 3 && i < 9) {
+                    continue;
+                }
+                System.out.println(i);
+            }
+
+        }
+
+    }
