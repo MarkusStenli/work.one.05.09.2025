71b5bbc8d8ac417c7c2e9a029a251f10429bf3e4
Markus <marku.stenli.47@gmail.com>
2025-09-08 23:17:19 +0300

    поправка

--- src/main/java/ru/otus/java/basic/home/work/Application.java
+++ src/main/java/ru/otus/java/basic/home/work/Application.java
@@ -2,8 +2,10 @@ package ru.otus.java.basic.home.work;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println();
        one();
        two();
+       three();
         }
 
         public static void one () {
@@ -12,14 +14,22 @@ public class Application {
              }
         }
 
-        public static void two (){
-            for (int i = 1; i <= 10; i++) {
-                if (i > 3 && i < 9) {
-                    continue;
-                }
+       public static void two (){
+            for (int i = 20; i <= 50; i++) {
+                if (i % 2 == 0) {
                 System.out.println(i);
+                }
             }
 
+        }
+
+        public static void three (){
+        for (int j = 1; j <= 10; j++){
+            if (j > 4 && j < 8){
+                continue;
+            }
+            System.out.println(j);
+        }
         }
 
     }
