6f896612dd86691b22d62361360895eb1f9628a6
Markus <marku.stenli.47@gmail.com>
2025-09-09 00:30:17 +0300

    упражнение five понял

--- src/main/java/ru/otus/java/basic/home/work/Application.java
+++ src/main/java/ru/otus/java/basic/home/work/Application.java
@@ -1,11 +1,15 @@
 package ru.otus.java.basic.home.work;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println();
        one();
        two();
        three();
+       four();
+       five();
         }
 
         public static void one () {
@@ -15,9 +19,9 @@ public class Application {
         }
 
        public static void two (){
-            for (int i = 20; i <= 50; i++) {
+            for (int i = 20; i <= 30; i++) {
                 if (i % 2 == 0) {
-                System.out.println(i);
+                System.out.print(" " + i + " " );
                 }
             }
 
@@ -25,11 +29,28 @@ public class Application {
 
         public static void three (){
         for (int j = 1; j <= 10; j++){
-            if (j > 4 && j < 8){
+            if (j > 2 && j < 8){
                 continue;
             }
             System.out.println(j);
         }
+        }
+
+        public static void four (){
+        for (int m = 0; m < 2; m++) {
+            for (int k = 0; k < 5; k++) {
+                System.out.print(" " + m + k);
+            }
+        }
+        }
+
+        public static void five (){
+            Scanner scanner = new Scanner(System.in);
+        int min = scanner.nextInt(), max = scanner.nextInt();
+        for (int i = min; i <= max; i++){
+            System.out.println("PRINT " + i);
+        }
+
         }
 
     }
